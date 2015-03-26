a94a4cb2833755a8ce3f7db8a61cc2b374a624a4
dotcz12 <dotcz12@login.rustler.tacc.utexas.edu>
2015-03-26 09:40:09 -0500

    Updated with number of mapper/reducers

--- RHIPE/wordcount.sh
+++ RHIPE/wordcount.sh
@@ -7,13 +7,31 @@
 
 library(Rhipe)
 rhinit()
-#rhoptions(runner=paste(getwd(),'/rhipe-runner.sh',sep=''))
 rhoptions(runner = 'sh /home/dotcz12/R/lib64/R/library/Rhipe/bin/RhipeMapReduce.sh')
 
-input.file.local <- 'sample.txt'
-input.file.hdfs <- paste('/user/',Sys.getenv('USER'),'/sample.txt',sep='')
-output.dir.hdfs <- paste('/user/',Sys.getenv('USER'),'/sample-txt-out',sep='')
-output.file.local <- 'output_sample.txt'
+#input.file.local <- 'sample.txt'
+input.user.dir <- paste('/user/',Sys.getenv('USER'),sep='')
+
+args=commandArgs(trailingOnly = TRUE)
+
+rhclean()
+
+if(length(args) < 2){
+  input.file.name<- 'book100.txt'
+ # input.file.name <- 'enwiki-latest-pages-meta-current1.xml'
+  input.testnum<-1
+  num_reducers<-0
+  num_mappers<-10
+}else{
+  input.testnum <- args[[1]] 
+  input.file.name <- args[[2]]
+  num_reducers<- args[[4]]
+  num_mappers<- args[[3]]
+}
+
+input.file.hdfs <- paste(input.user.dir,'/data/',input.file.name,sep='')
+output.dir.hdfs <- paste(input.user.dir,'/out/',format(Sys.time(), "%H%M%OS"),input.file.name,sep='')
+output.file.local <- paste('rhipe_',input.file.name,num_mappers,'M',num_reducers,'R',sep='')
 
 mapper <- expression( {
     # 'map.values' is a list containing each line of the input file
@@ -41,15 +59,32 @@ reducer <- expression(
 )
 
 # equivalent to hadoop dfs -copyFromLocal
-rhput(input.file.local, input.file.hdfs)
-
+#rhput(input.file.local, input.file.hdfs)
+# -Dmapred.job.name="$TEST-$MAPPER_COUNT-$REDUCER_COUNT-$INPUT" \
+# -Dmapreduce.job.maps=$MAPPER_COUNT \
+# -Dmapreduce.job.reduces=$REDUCER_COUNT \
+# -Dmapreduce.map.java.opts=-Xmx12000M \
+# -Dmapreduce.reduce.java.opts=-Xmx12000M \
 # rhwatch launches the Hadoop job
+#            , mapred.map.tasks=num_mappers #CDH3,4
+#           , mapred.reduce.tasks=num_reducers #CDH3,4
+ 
+ mapred = list(
+            mapred.task.timeout=0
+            , mapreduce.job.maps=num_mappers #CDH5
+            , mapreduce.job.reduces= num_reducers #CDH5
+        )
 rhipe.results <- rhwatch(
                         map=mapper, reduce=reducer,
                         input=rhfmt(input.file.hdfs, type="text"),
                         output=output.dir.hdfs,
-                        jobname='Wordcount',
-                        mapred=list(mapred.reduce.tasks=2))
+                        jobname=paste("rhipe-",num_mappers,num_reducers,input.file.name,sep="-"),
+                        mapred=mapred)
+
+                        # mapred=list(paste("mapreduce.job.maps",num_mappers,sep='='),
+            #, mapred.job.reuse.jvm.num.tasks=-1
+            #, mapreduce.job.jvm.numtasks=-1
+                        #   paste("mapreduce.job.reduces",num_reducers,sep='=')))
 # the mapred=... parameter is optional in rhwatch() above
 
 # results on HDFS are in Rhipe object binary format, NOT ASCII, and must be
