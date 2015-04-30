7d2683b95851e494194359ac32645a129926efee
Marek <marek.bejda@gmail.com>
2015-04-30 16:05:36 -0500

    kmeans finally working, just no output

--- RHIPE/kmeans.sh
+++ RHIPE/kmeans.sh
@@ -12,8 +12,7 @@ kmeans_test <- function(data_size, num_clusters, num_iterations, directory) {
     #Create data and move data to HDFS
    # input <- do.call(rbind,rep(list(matrix(rnorm(10*data_size, sd=10), ncol=2)), 20))    + matrix(rnorm(200), ncol=2)
    input<-list()
-   for(i in 1:10) input<-c(input,list(list(rnorm(1, sd=10),rnorm(1,sd=10))));
-  rhdel(directory)
+   for(i in 1:(10*data_size)) input<-c(input,list(list(rnorm(1, sd=10),rnorm(1,sd=10))));
    rhwrite(input, file=directory) 
     
     #Helper function: Euclidian distance
@@ -42,20 +41,20 @@ kmeans_test <- function(data_size, num_clusters, num_iterations, directory) {
                 nearest <- max.col(-distance)
             }
         }
-        rhcollect(nearest,P)
-        #lapply(nearest, FUN=rhcollect, value=P)
+        #rhcollect(nearest,P)
+        lapply(nearest, FUN=rhcollect, value=P)
     }))
 
     #Reduce: Compute new centroids
     reducer <- expression(
-        pre = {
+        pre = function(reduce){
             total = NULL
         },
-    	reduce = {
+    	reduce = function(reduce){
             total <- if(total == NULL) (as.matrix(lapply(reduce.values, sum)))
          else (as.matrix(lapply(rbind(total, reduce.values), 2, sum)))
          }, 
-         post = {
+         post = function(reduce){
             rhcollect(reduce.key, total)
             # lapply(reduce.key, FUN=rhcollect, value=total)
         }
@@ -63,19 +62,29 @@ kmeans_test <- function(data_size, num_clusters, num_iterations, directory) {
 
     
     C = NULL
-    # for(i in 1:num_iterations ) {
+      for(i in 1:num_iterations ) {
     job <- rhwatch(
         map=mapper,
         reduce = reducer,
         input=rhfmt(directory,type="sequence"), 
-        output="hdfs:///user/dotcz12/outkmeans",
+        output="hdfs:///user/dotcz12/kmeansout",
         mapred=NULL,
         jobname="kmeans",
         verbose=TRUE) 
         # rhex(job)  
-      # C = rhread("hdfs:///user/dotcz12/outkmeans")
-    # }
-    # C
+        C = rhread("hdfs:///user/dotcz12/kmeansout")
+      }
+        write.table( data.frame(words=unlist(lapply(X=C,FUN="[[",1)),
+                          count=unlist(lapply(X=C,FUN="[[",2))),
+               file="/home/dotcz12/out/brokenfile",
+               quote=FALSE,
+                 append=TRUE,
+               row.names=FALSE,
+               col.names=FALSE,
+               sep="\t"
+               )
 }
 
-kmeans_test(1,5,5, 'hdfs:///user/dotcz12/kmeans')
+directory<-"hdfs:///user/dotcz12/kmeans"
+rhdel(directory)
+kmeans_test(1,5,5, directory)
